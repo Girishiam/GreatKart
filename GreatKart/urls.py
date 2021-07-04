c25f9f0f1c9c4113788f181cec8ea9d6bb71ccd5
Girish <girishmondal.28@gmail.com>
2021-07-04 14:56:12 +0600

    store and carts functionality

--- GreatKart/urls.py
+++ GreatKart/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('store/', include('store.urls')),
+    path('cart/', include('cart.urls')),
 ]
 
 
--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'store'
 urlpatterns = [
     path('', views.store_page, name='store'),
-    path('<slug:slug>/', views.store_page, name='products_by_category'),
-    path('<slug:slug>/<slug:product_slug>',
+    path('category/<slug:slug>/', views.store_page, name='products_by_category'),
+    path('category/<slug:slug>/<slug:product_slug>',
          views.product_details, name='productdetails'),
+    path('search/', views.search, name='search'),
 ]
