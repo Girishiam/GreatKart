64efe5200fc8cc4c63d07349ae193e60bb9bd5c2
Girish <girishmondal.28@gmail.com>
2021-07-24 13:07:26 +0600

    django authentication

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -138,3 +139,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP_configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'girishmondal.28@gmail.com'
+EMAIL_HOST_PASSWORD = 'xyiMgirish28@'
+EMAIL_USE_TLS = True
